8de456a5271c080ce2927338f4f95cc2d4e5c952
starkwilly <williamcod93@gmail.com>
2023-08-08 13:58:57 -0600

    API response
    file to receive and interpret the response from the API

---
+++ import xml.etree.py
@@ -0,0 +1,20 @@
+import xml.etree.ElementTree as ET
+
+# Assuming you already have the 'response_xml' from the previous example
+
+# Parse the XML response
+root = ET.fromstring(response_xml)
+
+# Extract information from the parsed XML
+vehicle_type = root.find(".//Type").text
+vehicle_make = root.find(".//Make").text
+max_passengers = root.find(".//MaxPassengers").text
+num_doors = root.find(".//NumDoors").text
+vehicle_photo_url = root.find(".//PhotoURL").text
+
+# Print the extracted information
+print("Vehicle Type:", vehicle_type)
+print("Vehicle Make:", vehicle_make)
+print("Max Passengers:", max_passengers)
+print("Number of Doors:", num_doors)
+print("Vehicle Photo URL:", vehicle_photo_url)
